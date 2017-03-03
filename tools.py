f67e0c870cdef9117ea77927370f9edb973443a3
Louis Rémus <15720130+louis-r@users.noreply.github.com>
2017-03-03 16:01:16 +0100

    graph dirty in graph.ipynb, intoutdegree added in feature_extractor, illustrated in feature_generation (serves as main.py)

--- tools.py
+++ tools.py
@@ -1,4 +1,36 @@
 import numpy as np
+import igraph
+import csv
+
+def build_graph():
+    with open("testing_set.txt", "r") as f:
+        reader = csv.reader(f)
+        testing_set  = list(reader)
+    testing_set = [element[0].split(" ") for element in testing_set]
+
+    with open("training_set.txt", "r") as f:
+        reader = csv.reader(f)
+        training_set  = list(reader)
+
+    training_set = [element[0].split(" ") for element in training_set]
+
+    with open("node_information.csv", "r") as f:
+        reader = csv.reader(f)
+        node_info  = list(reader)
+
+    IDs = [element[0] for element in node_info]
+    edges = [(element[0],element[1]) for element in training_set if element[2]=="1"]
+    ## some nodes may not be connected to any other node
+    ## hence the need to create the nodes of the graph from node_info.csv,
+    ## not just from the edge list
+    nodes = IDs
+    ## create empty directed graph
+    g = igraph.Graph(directed=True)
+    ## add vertices
+    g.add_vertices(nodes)
+    ## add edges
+    g.add_edges(edges)
+    return g
 
 def random_sample(df, p = 0.05):
     '''
