b18f601df1fca70c7b80bd072365476d668983a7
Edouard360 <mailedouardmehlman@gmail.com>
2017-03-03 18:27:29 +0100

    Abstract to graph analysis (TD3)

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,12 +1,14 @@
 import numpy as np
-import nltk
+from tools import remove_stopwords_and_stem
+import csv
+import pandas as pd
+from scipy import sparse
+from library_moodle import terms_to_graph,compute_node_centrality
+from sklearn.feature_extraction.text import TfidfVectorizer
+import matplotlib.pyplot as plt
 
-stpwds = set(nltk.corpus.stopwords.words("english"))
-stemmer = nltk.stem.PorterStemmer()
-
-# from sklearn.feature_extraction.text import TfidfVectorizer
 # vectorizer = TfidfVectorizer(stop_words="english")
-# features_TFIDF = vectorizer.fit_transform(node_information_df["abstract"])
+# features_TFIDF = vectorizer.fit_transform(df["abstract"].values)
 
 def featuresFromDataFrame(df,node_information_df,verbose = True):
     '''
@@ -23,12 +25,10 @@ def featuresFromDataFrame(df,node_information_df,verbose = True):
         target_info = node_information_df.ix[target, :]
 
         source_title = source_info["title"].lower().split(" ")
-        source_title = [token for token in source_title if token not in stpwds]
-        source_title = [stemmer.stem(token) for token in source_title]
+        source_title = remove_stopwords_and_stem(source_title)
 
         target_title = target_info["title"].lower().split(" ")
-        target_title = [token for token in target_title if token not in stpwds]
-        target_title = [stemmer.stem(token) for token in target_title]
+        target_title = remove_stopwords_and_stem(target_title)
 
         source_auth = source_info["authors"].split(",")
         target_auth = target_info["authors"].split(",")
@@ -40,3 +40,17 @@ def featuresFromDataFrame(df,node_information_df,verbose = True):
         if verbose and (counter % 1000 == True):
             print(counter, " examples processed")
     return np.array([overlap_title, temp_diff, comm_auth]).T
+
+def addFeaturesFromMetric(node_information_df, metric = "degrees", percentile = 95):
+    loader = np.load("preprocessing/node-abstract/node-feature-"+metric+".npz")
+    features = sparse.csr_matrix((loader['data'], loader['indices'], loader['indptr']),
+                          shape=loader['shape'])
+    E_M2 = np.array((features).multiply(features).mean(axis=0))
+    E_M_2 = np.array((features).mean(axis=0))**2
+    var = (E_M2-E_M_2)[0]
+
+    # plt.plot(range(100), [np.percentile(var, i) for i in range(100)])
+    features_to_keep = (var >= np.percentile(var, percentile))
+    features = features[:, features_to_keep]
+
+    return pd.concat((node_information_df,pd.DataFrame(index = node_information_df.index, data = features.toarray(),columns = [metric+"_"+str(i) for i in range(features.shape[1])])),axis=1)
--- tools.py
+++ tools.py
@@ -1,10 +1,19 @@
 import numpy as np
+import nltk
 
-def random_sample(df, p = 0.05):
+stpwds = set(nltk.corpus.stopwords.words("english"))
+stemmer = nltk.stem.PorterStemmer()
+
+def remove_stopwords_and_stem(words):
+    words = [token for token in words if (len(token)>2 and (token not in stpwds))]
+    return [stemmer.stem(token) for token in words]
+
+def random_sample(df, p = 0.05,seed=42):
     '''
     Randomly samples a proportion 'p' of rows of a dataframe
     '''
     size = df.shape[0]
+    np.random.seed(seed)
     return df.ix[np.random.randint(0, size, int(size*p)), :]
 
 def stats_df(df):
