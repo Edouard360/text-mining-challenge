79a1ecaa7c286c44c8d7535753cde732b13b09c2
Edouard360 <mailedouardmehlman@gmail.com>
2017-03-12 00:58:28 +0100

    Authors maximum exploitation

--- preprocessing/authorsFeatures/AuthorsFeatureExtractor.py
+++ preprocessing/authorsFeatures/AuthorsFeatureExtractor.py
@@ -1,52 +1,25 @@
 from preprocessing.FeatureExtractor import FeatureExtractor
 import numpy as np
 import pandas as pd
-from tools import authors_citation_dict, authors_citation_graph
+from tools import authors_citation_dict, authors_citation_graph, authors_collaboration_graph
 
 
 class AuthorsFeatureExtractor(FeatureExtractor):
+    columns = ["meanACiteB_col", "maxACiteB_col",
+               "AOut_col", "BIn_col",
+               "ACiteAMean_col", "ACiteASum_col",
+               "BOut_col"
+               # "meanACollaborateWithB_col", "maxACollaborateWithB_col",
+               # "dice_collaboration_mean_col", "dice_collaboration_max_col",
+               # "jaccard_collaboration_mean_col", "jaccard_collaboration_max_col"
+               ]
+
     def __init__(self, node_information_df, verbose=False, freq=10000, **kargs):
         super(AuthorsFeatureExtractor, self).__init__(node_information_df, verbose=verbose, freq=freq)
         self.id_to_index = dict(zip(self.node_information_df.index.values, range(self.node_information_df.index.size)))
         print("Building authors citation graph")
         self.authors_citation_graph = authors_citation_graph()
-
-        # try:
-        #     meanWsAuthorsCitation_df = pd.read_csv("preprocessing/authorsFeatures/meanWsAuthorsCitation.csv")
-        # except FileNotFoundError:
-        #     print("Building authors graph for the authorFeature")
-        #     dict_authors_edges = authors_citation_dict()
-        #     train_df = pd.read_csv("data/training_set.txt", sep=" ", header=None, usecols=[0,1])
-        #     train_df.columns = ["source", "target"]
-        #     test_df = pd.read_csv("data/testing_set.txt", sep=" ", header=None)
-        #     test_df.columns = ["source", "target"]
-        #     concatenated_df = pd.concat((train_df,test_df),axis=0)
-        #     list_concat = concatenated_df.values.tolist()
-        #     authors = node_information_df["authors"].values.tolist()
-        #     authors = [author_list.split(", ") for author_list in authors]
-        #     authors = [list(filter(None, author_list)) for author_list in authors]
-        #     meanWsAuthorsCitation = []
-        #     print("Computing meanWsAuthorsCitation list")
-        #     for source, target in list_concat:
-        #         quotes = []
-        #         source_authors = authors[self.id_to_index[source]]
-        #         target_authors = authors[self.id_to_index[target]]
-        #         for source_author in source_authors:
-        #             for target_author in target_authors:
-        #                 if (source_author!= target_author) and (source_author, target_author) in dict_authors_edges:
-        #                     quotes.append(dict_authors_edges[(source_author, target_author)])
-        #
-        #         if quotes:
-        #             meanWsAuthorsCitation.append(np.mean(quotes))
-        #         else:
-        #             meanWsAuthorsCitation.append(0)
-        #     concatenated_df["meanWsAuthorsCitation"] = meanWsAuthorsCitation
-        #     print("Exporting concatenated authorsFeatures to preprocessing/authorsFeatures/meanWsAuthorsCitation.csv")
-        #     concatenated_df.to_csv("preprocessing/authorsFeatures/meanWsAuthorsCitation.csv", index=False)
-        #     meanWsAuthorsCitation_df = pd.read_csv("preprocessing/authorsFeatures/meanWsAuthorsCitation.csv")
-        # self.meanWsAuthorsCitation_df = meanWsAuthorsCitation_df.set_index(["source","target"])
-
-
+        # self.authors_collaboration_graph = authors_collaboration_graph()
         authors = self.node_information_df["authors"].values.tolist()
         print("Splitting authors list")
         authors = [author_list.split(", ") for author_list in authors]
@@ -55,28 +28,78 @@ class AuthorsFeatureExtractor(FeatureExtractor):
         print("Authors name to id")
         self.authors_list_id = [[self.authors_citation_graph["authors_to_index"][author] for author in author_list] for
                                 author_list in authors]
-        self.mean_authors_citation = []
+        self.meanACiteB_col, self.maxACiteB_col, self.meanACollaborateWithB_col, self.maxACollaborateWithB_col = [], [], [], []
+        self.meanACiteB_col, self.maxACiteB_col, self.meanACollaborateWithB_col, self.maxACollaborateWithB_col = [], [], [], []
+        self.AOut_col, self.BIn_col = [], []
+        self.ACiteASum_col, self.ACiteAMean_col = [], []
+        self.BOut_col = []
+        # self.dice_collaboration_mean_col, self.dice_collaboration_max_col,\
+        # self.jaccard_collaboration_mean_col,self.jaccard_collaboration_max_col = [],[],[],[]
 
     def reset(self):
-        self.mean_authors_citation = []
+        self.meanACiteB_col, self.maxACiteB_col, self.meanACollaborateWithB_col, self.maxACollaborateWithB_col = [], [], [], []
+        self.AOut_col, self.BIn_col = [], []
+        self.ACiteASum_col, self.ACiteAMean_col = [], []
+        self.BOut_col = []
+        # self.dice_collaboration_mean_col, self.dice_collaboration_max_col,\
+        # self.jaccard_collaboration_mean_col,self.jaccard_collaboration_max_col = [],[],[],[]
 
     def extractStep(self, source, target):
         source_authors = self.authors_list_id[self.id_to_index[source]]
         target_authors = self.authors_list_id[self.id_to_index[target]]
+
+        ACiteB = self.authors_citation_graph.es.select(_between=(source_authors, target_authors))
+        AOut = self.authors_citation_graph.outdegree(source_authors)
+        BIn = self.authors_citation_graph.indegree(target_authors)
+        BOut = self.authors_citation_graph.outdegree(target_authors)
+        # edgeSeqCollaboration = self.authors_collaboration_graph.es.select(_between=(source_authors, target_authors))
+
         # pairs = [(source,target) for source in source_authors for target in target_authors if source!=target]
+        # dice_collaboration = self.authors_collaboration_graph.similarity_dice(pairs=pairs)
+        # jaccard_collaboration = self.authors_collaboration_graph.similarity_jaccard(pairs=pairs)
 
-        edgeSeq = self.authors_citation_graph.es.select(_between=(source_authors, target_authors))
+        common_authors = list(set(source_authors) & set(target_authors))
+        ACiteA = self.authors_citation_graph.vs[common_authors]["weight"]
+        self.ACiteAMean_col.append(0 if len(ACiteA) == 0 else np.mean(ACiteA))
+        self.ACiteASum_col.append(0 if len(ACiteA) == 0 else np.sum(ACiteA))
 
-        # np.mean(self.authors_citation_graph.similarity_dice(pairs=pairs))
-        # self.authors_citation_graph.get_eids()
-        if (len(edgeSeq["weight"]) > 0):
-            mean = np.mean(edgeSeq["weight"])
+        if (len(ACiteB) > 0):
+            meanACiteB = np.mean(ACiteB["weight"])
+            maxACiteB = np.max(ACiteB["weight"])
         else:
-            mean = 0
+            meanACiteB = 0
+            maxACiteB = 0
+        self.meanACiteB_col.append(meanACiteB)
+        self.maxACiteB_col.append(maxACiteB)
 
-        self.mean_authors_citation.append(mean)
+        self.AOut_col.append(0 if len(AOut) == 0 else np.mean(AOut))
+        self.BIn_col.append(0 if len(BIn) == 0 else np.mean(BIn))
+        self.BOut_col.append(0 if len(BOut) == 0 else np.mean(BOut))
+
+        # if (len(edgeSeqCollaboration) > 0):
+        #     meanACollaborateWithB = np.mean(edgeSeqCollaboration["weight"])
+        #     maxACollaborateWithB = np.max(edgeSeqCollaboration["weight"])
+        # else:
+        #     meanACollaborateWithB = 0
+        #     maxACollaborateWithB = 0
+
+
+        # self.meanACollaborateWithB_col.append(meanACollaborateWithB)
+        # self.maxACollaborateWithB_col.append(maxACollaborateWithB)
+
+        # self.dice_collaboration_mean_col.append(0 if len(dice_collaboration)==0 else np.mean(dice_collaboration))
+        # self.dice_collaboration_max_col.append(0 if len(dice_collaboration)==0 else np.max(dice_collaboration))
+        # self.jaccard_collaboration_mean_col.append(0 if len(dice_collaboration)==0 else np.mean(jaccard_collaboration))
+        # self.jaccard_collaboration_max_col.append(0 if len(dice_collaboration)==0 else np.max(jaccard_collaboration))
 
         # self.mean_authors_citation.append(self.meanWsAuthorsCitation_df.loc[(source, target)].values[0])
 
     def concatFeature(self):
-        return np.array([self.mean_authors_citation]).T
+        return np.array([self.meanACiteB_col, self.maxACiteB_col,
+                         self.AOut_col, self.BIn_col,
+                         self.ACiteAMean_col, self.ACiteASum_col,
+                         self.BOut_col
+                         # self.meanACollaborateWithB_col, self.maxACollaborateWithB_col,
+                         # self.dice_collaboration_mean_col, self.dice_collaboration_max_col,
+                         # self.jaccard_collaboration_mean_col, self.jaccard_collaboration_max_col
+                         ]).T
--- preprocessing/commonNeighboursFeatures/commonNeighboursExtractor.py
+++ preprocessing/commonNeighboursFeatures/commonNeighboursExtractor.py
@@ -4,9 +4,12 @@ import pandas as pd
 from tools import build_graph
 import igraph
 
+
 class CommonNeighboursFeatureExtractor(FeatureExtractor):
-    def __init__(self, node_information_df, verbose = False, freq = 10000, **kargs):
-        super(CommonNeighboursFeatureExtractor, self).__init__(node_information_df,verbose = verbose, freq = freq)
+    columns = ["commonNeighbours"]
+
+    def __init__(self, node_information_df, verbose=False, freq=10000, **kargs):
+        super(CommonNeighboursFeatureExtractor, self).__init__(node_information_df, verbose=verbose, freq=freq)
         self.id_to_index = dict(zip(self.node_information_df.index.values, range(self.node_information_df.index.size)))
         try:
             common_neighbours_df = pd.read_csv("preprocessing/commonNeighboursFeatures/common_neighbours.csv")
@@ -14,28 +17,28 @@ class CommonNeighboursFeatureExtractor(FeatureExtractor):
             print("Building graph for the commonNeighboursFeature")
             g = build_graph()
 
-            train_df = pd.read_csv("data/training_set.txt", sep=" ", header=None, usecols=[0,1])
+            train_df = pd.read_csv("data/training_set.txt", sep=" ", header=None, usecols=[0, 1])
             train_df.columns = ["source", "target"]
             test_df = pd.read_csv("data/testing_set.txt", sep=" ", header=None)
             test_df.columns = ["source", "target"]
-            concatenated_df = pd.concat((train_df,test_df),axis=0)
+            concatenated_df = pd.concat((train_df, test_df), axis=0)
             list_concat = concatenated_df.values.tolist()
             list_concat = [tuple(concat) for concat in list_concat]
             print("Building list_concat")
             list_concat = [tuple([self.id_to_index[concat[0]], self.id_to_index[concat[1]]]) for concat in list_concat]
             print("Similarity Dice computation")
             concatenated_df["commonNeighbours"] = g.similarity_dice(pairs=list_concat)
-            concatenated_df.to_csv("preprocessing/commonNeighboursFeatures/common_neighbours.csv",index=False)
+            concatenated_df.to_csv("preprocessing/commonNeighboursFeatures/common_neighbours.csv", index=False)
             print("Exporting graph to preprocessing/commonNeighboursFeatures/common_neighbours.csv")
             common_neighbours_df = pd.read_csv("preprocessing/commonNeighboursFeatures/common_neighbours.csv")
-        self.common_neighbours_df = common_neighbours_df.set_index(["source","target"])
+        self.common_neighbours_df = common_neighbours_df.set_index(["source", "target"])
         self.commonNeighbours = []
 
     def reset(self):
         self.commonNeighbours = []
 
     def extractStep(self, source, target):
-        self.commonNeighbours.append(self.common_neighbours_df.loc[(source ,target)].values[0])
+        self.commonNeighbours.append(self.common_neighbours_df.loc[(source, target)].values[0])
 
     def concatFeature(self):
         return np.array([self.commonNeighbours]).T
