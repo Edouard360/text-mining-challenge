92fdcf011e550653fe5db30336ab84a28b1db2ce
Louis Rémus <15720130+louis-r@users.noreply.github.com>
2017-03-13 19:38:58 +0100

    Added pagerank feature for the target

--- preprocessing/FeatureExporter.py
+++ preprocessing/FeatureExporter.py
@@ -4,6 +4,7 @@ from preprocessing.originalFeatures.OriginalFeatureExtractor import OriginalFeat
 from preprocessing.inOutFeatures.InOutFeatureExtractor import InOutFeatureExtractor
 from preprocessing.commonNeighboursFeatures.commonNeighboursExtractor import CommonNeighboursFeatureExtractor
 from preprocessing.intersectionFeatures.IntersectionFeatureExtractor import IntersectionFeatureExtractor
+from preprocessing.pageRankFeatures.PageRankFeatureExtractor import PageRankFeatureExtractor
 from preprocessing.tfidfFeatures.TfidfFeatureExtractor import TfidfFeatureExtractor
 from preprocessing.authorsFeatures.AuthorsFeatureExtractor import AuthorsFeatureExtractor
 
@@ -50,6 +51,12 @@ class FeatureExporter:
             "columns": AuthorsFeatureExtractor.columns,
             "path": "authorsFeatures/",
             "extractor": AuthorsFeatureExtractor,
+            "default_args": {}
+        },
+        "pageRank": {
+            "columns": PageRankFeatureExtractor.columns,
+            "path": "pageRankFeatures/",
+            "extractor": PageRankFeatureExtractor,
             "default_args": {}
         }
     }
