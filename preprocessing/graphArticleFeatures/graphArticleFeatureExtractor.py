22c16e399bdb16ab7d255f81fefd1e782fbf7e23
Louis Rémus <15720130+louis-r@users.noreply.github.com>
2017-03-16 20:00:32 +0100

    moving inoutfeature to grapharticle feature

--- preprocessing/graphArticleFeatures/graphArticleFeatureExtractor.py
+++ preprocessing/graphArticleFeatures/graphArticleFeatureExtractor.py
@@ -2,11 +2,11 @@ from preprocessing.FeatureExtractor import FeatureExtractor
 import numpy as np
 import pandas as pd
 from tools import articles_graph
-import igraph
 
 
 class GraphArticleFeatureExtractor(FeatureExtractor):
-    columns = ["commonNeighbours", "coreness_in_f", "coreness_out_f", "coreness_all_f", "id_to_cluster_max", "pagerank"]
+    columns = ["indegree", "outdegree", "commonNeighbours", "coreness_in_f", "coreness_out_f", "coreness_all_f",
+               "id_to_cluster_max", "pagerank"]
 
     def __init__(self, node_information_df, verbose=False, freq=10000, **kargs):
         super(GraphArticleFeatureExtractor, self).__init__(node_information_df, verbose=verbose, freq=freq)
@@ -17,13 +17,24 @@ class GraphArticleFeatureExtractor(FeatureExtractor):
         for i, id_articles_list in enumerate(self.articles_graph.clusters(mode="WEAK")):
             for id_article in id_articles_list:
                 self.index_to_cluster[id_article] = i
+        self.node_degree_df = pd.DataFrame(
+            data={"ID": self.articles_graph.vs["name"],
+                  "indegree": self.articles_graph.indegree(),
+                  "outdegree": self.articles_graph.outdegree()}
+        )
+        # self.node_degree_df.columns = ["ID", "indegree", "outdegree"]
+        self.node_degree_df = self.node_degree_df.reset_index().set_index("ID")
+
         self.pagerank = self.articles_graph.pagerank()
         self.coreness_in = self.articles_graph.coreness(mode="in")
         self.coreness_out = self.articles_graph.coreness(mode="out")
         self.coreness_all = self.articles_graph.coreness(mode="all")
+        self.node_degree_df = self.node_degree_df[["indegree", "outdegree"]].values
         self.reset()
 
     def reset(self):
+        self.indegree = []
+        self.outdegree = []
         self.commonNeighbours = []
         self.coreness_in_f = []
         self.coreness_out_f = []
@@ -41,7 +52,9 @@ class GraphArticleFeatureExtractor(FeatureExtractor):
         self.coreness_all_f.append(self.coreness_all[index_source] - self.coreness_all[index_target])
         self.commonNeighbours.append(self.articles_graph.similarity_dice(pairs=[(index_source, index_target)])[0])
         self.pagerank_f.append(max(self.pagerank[index_source], self.pagerank[index_target]))
+        self.indegree.append(self.node_degree_df[self.id_to_index[target], 0])
+        self.outdegree.append(self.node_degree_df[self.id_to_index[target], 1])
 
     def concatFeature(self):
         return np.array([self.commonNeighbours, self.coreness_in_f, self.coreness_out_f, self.coreness_all_f,
-                         self.id_to_cluster_max, self.pagerank_f]).T
+                         self.id_to_cluster_max, self.pagerank_f, self.indegree, self.outdegree]).T
