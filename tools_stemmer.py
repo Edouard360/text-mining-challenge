6cf0573b5532806df045ca47e8adf5ae6c94e50c
Edouard360 <mailedouardmehlman@gmail.com>
2017-09-23 10:50:49 +0200

    Elementary test

---
+++ tools_stemmer.py
@@ -0,0 +1,10 @@
+import nltk
+
+stpwds = set(nltk.corpus.stopwords.words("english"))
+stemmer = nltk.stem.PorterStemmer()
+
+
+def remove_stopwords_and_stem(words):
+    words = [token for token in words if (len(token) > 2 and (token not in stpwds))]
+    return [stemmer.stem(token) for token in words]
+
