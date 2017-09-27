1e7edcf805e5df1744b40ef52a8184cdbda3fe66
Louis Rémus <15720130+louis-r@users.noreply.github.com>
2017-09-26 23:13:26 -0700

    Deleted setup.py Why this change was necessary:
    * Useless since this is not a package
    
    This change addresses the need by:
    
    *
    
    Potential side-effects:
    
    *

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    install_requires=['docopt>=0.6.1', 'coverage>=3.6', 'requests>=1.0.0'],
-    setup_requires=['pytest-runner'],
-    tests_require=[ 'pytest']#'mock','sh>=1.08'
-)
-
